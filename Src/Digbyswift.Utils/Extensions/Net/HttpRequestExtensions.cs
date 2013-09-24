c462d8e6a4cd0f2c7e527808e17e975328fbb616
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2013-09-24 12:15:42 +0100

    Reverted from having separate HttpRequestBaseExtensions.cs

--- Src/Digbyswift.Utils/Extensions/Net/HttpRequestExtensions.cs
+++ Src/Digbyswift.Utils/Extensions/Net/HttpRequestExtensions.cs
@@ -11,15 +11,10 @@ namespace Digbyswift.Utils.Extensions.Net
             public static string RemoteAddr = "REMOTE_ADDR";
         }
 
+
         public static string GetCurrentIp(this HttpRequest value)
         {
-            string workingIp = value.ServerVariables[ServerVariables.HttpXForwardedFor];
-
-            //If there is no proxy, get the standard remote address
-            if (workingIp.TrimToNull() != null && workingIp.ToUpperInvariant() != "UNKNOWN")
-                return workingIp;
-
-            return value.ServerVariables[ServerVariables.RemoteAddr];
+            return new HttpRequestWrapper(value).GetCurrentIp();
         }
 
         public static string GetCurrentIp(this HttpRequestBase value)
@@ -33,25 +28,43 @@ namespace Digbyswift.Utils.Extensions.Net
             return value.ServerVariables[ServerVariables.RemoteAddr];
         }
 
-		/// <summary>
-		/// Specifies whether the current HttpRequest object has a Referrer
-		/// </summary>
-		public static bool HasReferrer(this HttpRequest value)
-		{
-			return value.UrlReferrer != null;
-		}
+        /// <summary>
+        /// Specifies whether the current HttpRequest object has a Referrer
+        /// </summary>
+        public static bool HasReferrer(this HttpRequest value)
+        {
+            return new HttpRequestWrapper(value).HasReferrer();
+        }
 
-		/// <summary>
-		/// Returns true if the the current HttpRequest object's referrer is
-		/// internal. Will return false if no referrer exists.
-		/// </summary>
-		public static bool HasInternalReferrer(this HttpRequest value)
-		{
-			if (!value.HasReferrer())
-				return false;
+        /// <summary>
+        /// Specifies whether the current HttpRequest object has a Referrer
+        /// </summary>
+        public static bool HasReferrer(this HttpRequestBase value)
+        {
+            return value.UrlReferrer != null;
+        }
 
-			return value.UrlReferrer.DnsSafeHost == value.Url.DnsSafeHost;
-		}
+        /// <summary>
+        /// Returns true if the the current HttpRequest object's referrer is
+        /// internal. Will return false if no referrer exists.
+        /// </summary>
+        public static bool HasInternalReferrer(this HttpRequest value)
+        {
+            return new HttpRequestWrapper(value).HasInternalReferrer();
+        }
+        
+        /// <summary>
+        /// Returns true if the the current HttpRequest object's referrer is
+        /// internal. Will return false if no referrer exists.
+        /// </summary>
+        public static bool HasInternalReferrer(this HttpRequestBase value)
+        {
+            if (!value.HasReferrer())
+                return false;
+
+            return value.UrlReferrer.DnsSafeHost == value.Url.DnsSafeHost;
+        }
+
 
 	}
 }
